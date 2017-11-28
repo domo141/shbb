17de27d8fd63c5df71e2ec649c85661ec54635e5
Tomi Ollila <too@iki.fi>
2017-11-28 21:30:59 +0200

    c1-tmpl: use #pragma's for more flexibility in warning(/error) rules

--- templates/c1-tmpl.c
+++ templates/c1-tmpl.c
@@ -1,24 +1,74 @@
 #if 0 /* -*- mode: c; c-file-style: "stroustrup"; tab-width: 8; -*-
  set -euf; trg=${0##*''/}; trg=${trg%.c}; test ! -e "$trg" || rm "$trg"
- WARN="-Wall -Wstrict-prototypes -Winit-self -Wformat=2" # -pedantic
- WARN="$WARN -Wcast-align -Wpointer-arith " # -Wfloat-equal #-Werror
- WARN="$WARN -Wextra -Wwrite-strings -Wcast-qual -Wshadow" # -Wconversion
- WARN="$WARN -Wmissing-include-dirs -Wundef -Wbad-function-cast -Wlogical-op"
- WARN="$WARN -Waggregate-return -Wold-style-definition"
- WARN="$WARN -Wmissing-prototypes -Wmissing-declarations -Wredundant-decls"
- WARN="$WARN -Wnested-externs -Winline -Wvla -Woverlength-strings -Wpadded"
  case ${1-} in '') set x -O2; shift; esac
  #case ${1-} in '') set x -ggdb; shift; esac
- set -x; exec ${CC:-gcc} -std=c99 $WARN "$@" -o "$trg" "$0"
+ set -x; exec ${CC:-gcc} -std=c99 "$@" -o "$trg" "$0"
  exit $?
  */
 #endif
 
 // compile this as sh thisfile.c
 
-// a template for single file c project. warning options match gcc 4+
-// (and possibly clang) edit to suit your needs
-// (pragmas tbd, in another template)
+// a template for single file c project. warning options tested with
+// gcc 4.4.6 and newer (up to 7.x). more gcc 5+ options tbd (if any)
+// (clang defines some __GNUC__ value (4+?) ). edit to suit your needs
+
+// (Ø) public domain, like https://creativecommons.org/publicdomain/zero/1.0/
+
+#if 0 // change to '#if 1' whenever there is desire to see these...
+#pragma GCC diagnostic warning "-Wpadded"
+#pragma GCC diagnostic warning "-Wpedantic"
+#endif
+
+// gcc -dM -E -xc /dev/null | grep -i gnuc
+#if defined (__GNUC__)
+
+// to relax, change 'error' to 'warning' -- or even 'ignored'
+// selectively. use #pragma GCC diagnostic push/pop to change
+// the rules temporarily
+
+#pragma GCC diagnostic error "-Wall"
+#pragma GCC diagnostic error "-Wextra"
+
+#if __GNUC__ >= 7
+
+#pragma GCC diagnostic error "-Wimplicit-fallthrough"
+
+#endif /* __GNUC__ >= 7 */
+
+#pragma GCC diagnostic error "-Wstrict-prototypes"
+#pragma GCC diagnostic error "-Winit-self"
+
+// -Wformat=2 ¡currently! (2017-11) equivalent of the following 4
+#pragma GCC diagnostic error "-Wformat"
+#pragma GCC diagnostic error "-Wformat-nonliteral"
+#pragma GCC diagnostic error "-Wformat-security"
+#pragma GCC diagnostic error "-Wformat-y2k"
+
+#pragma GCC diagnostic error "-Wcast-align"
+#pragma GCC diagnostic error "-Wpointer-arith"
+#pragma GCC diagnostic error "-Wwrite-strings"
+#pragma GCC diagnostic error "-Wcast-qual"
+#pragma GCC diagnostic error "-Wshadow"
+#pragma GCC diagnostic error "-Wmissing-include-dirs"
+#pragma GCC diagnostic error "-Wundef"
+#pragma GCC diagnostic error "-Wbad-function-cast"
+#pragma GCC diagnostic error "-Wlogical-op"
+#pragma GCC diagnostic error "-Waggregate-return"
+#pragma GCC diagnostic error "-Wold-style-definition"
+#pragma GCC diagnostic error "-Wmissing-prototypes"
+#pragma GCC diagnostic error "-Wmissing-declarations"
+#pragma GCC diagnostic error "-Wredundant-decls"
+#pragma GCC diagnostic error "-Wnested-externs"
+#pragma GCC diagnostic error "-Winline"
+#pragma GCC diagnostic error "-Wvla"
+#pragma GCC diagnostic error "-Woverlength-strings"
+
+//ragma GCC diagnostic error "-Wfloat-equal"
+//ragma GCC diagnostic error "-Werror"
+//ragma GCC diagnostic error "-Wconversion"
+
+#endif /* defined (__GNUC__) */
 
 //#include <unistd.h>
 //#include <stdio.h>
