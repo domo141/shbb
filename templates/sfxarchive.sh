f0087bf1b25aace0c141da26424fac978bf111c9
Tomi Ollila <too@iki.fi>
2023-11-17 21:37:55 +0200

    sfxarchive.sh: suffix .(gz|xz|zstd) in last line to choose decompressor

--- templates/sfxarchive.sh
+++ templates/sfxarchive.sh
@@ -1,43 +1,51 @@
 #!/bin/sh
 #
 # Created: Wed 30 Apr 2003 11:41:13 EET too
-# Last Modified: Mon 22 May 2017 20:50:21 +0300 too
+# Last Modified: Fri 17 Nov 2023 21:34:13 +0200 too
 # Public Domain (this template)
 
 # This is self-extracting archive. You can use this to create new
 # self-extracting archives (cp to *.sfx and modify to suit your needs).
 # Be sure that tail(1) can handle binary content and long lines in your
 # target platform (at least when using +num -option).
-#
+
 # If you edit this script after you've catenated your archive, there is risk
 # that editor modifies the "binary". If editor adds (final) newline, you'll
 # get warning when extracting. You're more unfortunate if some changes are
 # made to it. Therefore I suggest do editing before archive is catenated.
 
+# The suffix in the last line, be it .gz (default), .xz or .zst is used to
+# choose the decompressor, gzip(1), xz(1) or zstd(1), respectively.
+
 # Some effort to keep this template traditional-shell compatible has been put.
-# E.g. $PWD may not work, replace with `pwd` if desired...
+# E.g. $PWD usually works, but if not, replace use of it with `pwd`.
 
 case ${BASH_VERSION-} in *.*) set -o posix; shopt -s xpg_echo; esac
 case ${ZSH_VERSION-} in *.*) emulate ksh; esac
 
-set -euf
+set -euf  # hint: (z|ba|da|'')sh -x thisfile [args] to trace execution
 
-LC_ALL=C LANG=C
-export LC_ALL LANG
+LC_ALL=C LANG=C; export LC_ALL LANG; unset LANGUAGE
 
 die () { echo "$@"; exit 1; } >&2
 
-# Find content separator
-SKIP=`exec awk '/^content[.]tar[.]gz\>/ { print NR + 1; exit 0; }' "$0"`
-
-test "$SKIP" || die "Cannot find content separator line 'content.tar.gz' in $0"
+# Find  c o n t e n t  separator
+SKIP=`grep -anm1 '^con't'ent.tar.' "$0"` ||
+	die "Cannot find con"t"ent separator line 'con"t"ent.tar...' in '$0'"
+case $SKIP
+	in *.gz)  uz=gzip
+	;; *.xz)  uz=xz
+	;; *.zst) uz=zstd
+	;; *) die "'Unknown con"t"ent separator line in '$0'"
+esac
+SKIP=${SKIP%:*}
+SKIP=$((SKIP + 1))
 
 # Absolute path to $0 -- so one may `cd` elsewhere before extraction...
 case $0 in /*) this=$0 ;; *) this=$PWD/$0 ;; esac
-#case $0 in /*) this=$0 ;; *) this=`pwd`/$0 ;; esac
 
 mk_tmpdir () {
-	tmpdir=`exec mktemp -d ${1:+$1/}_sfxa.XXXXXX`
+	tmpdir=`mktemp -d ${1:+$1/}_sfxa.XXXXXX`
 	trap "rm -rf $tmpdir" 0
 }
 
@@ -53,12 +61,12 @@ case ${1-} in -i) # fall through to installation part
 	exit
 
 ;; -v)	test $# = 1 || die "'-v' takes no arguments"
-	tail -n +$SKIP "$this" | gzip -dc | tar -tvf -
+	tail -n +$SKIP "$this" | $uz -dc | tar -tvf -
 	exit
 
 ;; -x)	test $# = 1 || die "'-x' takes no arguments"
 	mk_tmpdir
-	tail -n +$SKIP "$this" | gzip -dc | tar -C "$tmpdir" -xvvf -
+	tail -n +$SKIP "$this" | $uz -dc | tar -C "$tmpdir" -xvvf -
 	onefile=
 	onefile () {
 		test $# = 1 || return 0
@@ -95,7 +103,7 @@ esac
 
 # Here in case of -i were given on command line -- extract archive
 mk_tmpdir
-tail -n +$SKIP "$this" | gzip -dc | tar -C "$tmpdir" -xf -
+tail -n +$SKIP "$this" | $uz -dc | tar -C "$tmpdir" -xf -
 
 # Add commands to do after archive has been extracted here
 # ''''''''''''''''''''''''''''''''''''''''''''''''''''''''
